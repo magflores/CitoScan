c730fbbfb5fbb8d421c26bd51cb682ee2cfad3c5
magflores <magflores@itba.edu.ar>
2025-09-10 20:13:07 -0300

    Initial commit

---
+++ src/main/java/org/example/citoscan/CitoScanApplication.java
@@ -0,0 +1,13 @@
+package org.example.citoscan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitoScanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CitoScanApplication.class, args);
+    }
+
+}
